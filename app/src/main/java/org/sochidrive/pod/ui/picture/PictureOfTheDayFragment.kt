a5ea000aea805eb582c2eaf4d4ede09ef1d823a3
fadnier <fadnier@sochidrive.org>
2021-04-07 15:51:47 +0300

    homework2

--- app/src/main/java/org/sochidrive/pod/ui/picture/PictureOfTheDayFragment.kt
+++ app/src/main/java/org/sochidrive/pod/ui/picture/PictureOfTheDayFragment.kt
@@ -18,6 +18,7 @@ import kotlinx.android.synthetic.main.main_fragment.*
 import org.sochidrive.pod.MainActivity
 import org.sochidrive.pod.R
 import org.sochidrive.pod.ui.clip.ChipsFragment
+import org.sochidrive.pod.ui.settings.SettingsFragment
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -74,8 +75,8 @@ class PictureOfTheDayFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId) {
             R.id.app_bar_fav -> toast("Favorite")
-            R.id.app_bar_settings -> activity?.supportFragmentManager?.beginTransaction()?.add(R.id.container,
-                ChipsFragment())?.addToBackStack(null)?.commit()
+            R.id.app_bar_settings -> activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.container,
+                SettingsFragment())?.addToBackStack(null)?.commit()
             android.R.id.home -> {
                 activity?.let {
                     BottomNavigationDrawerFragment().show(it.supportFragmentManager,"tag")
