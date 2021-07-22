2c7929da03a83bebd0befe8860e72adf8eda8ded
fadnier <fadnier@sochidrive.org>
2021-07-22 18:24:02 +0300

    homework5 - start

---
+++ app/src/main/java/org/sochidrive/pod/ui/recycler/BaseViewHolder.kt
@@ -0,0 +1,8 @@
+package org.sochidrive.pod.ui.recycler
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class BaseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    abstract fun bind(dataItem: Pair<Data, Boolean>)
+}
